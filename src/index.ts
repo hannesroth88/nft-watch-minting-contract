6c25370d776cfbad43c7f0e652e3d314820f96d3
Hannes Roth <hannes.roth88@gmail.com>
2022-01-06 23:48:45 +0100

    getBalances from Zapper

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,13 @@ const axios = require('axios').default;
 require('dotenv').config()
 const Web3 = require('web3');
 
-enum TokenBalanceFrom {
-    Etherscan,
-    Zapper
-}
-
 const CONTRACTADDRESS = "0x9C38Bc76f282EB881a387C04fB67e9fc60aECF78"
 const ETHERSCANAPIKEY = process.env.ETHERSCANAPIKEY
 const INFURAAPIKEY = process.env.INFURA_API_KEY
 const MINT_FUNCTION_HEX = "0xa0712d68"
 const AMOUNT_TRANSACTION_SCANNED = 100
-const GET_TOKENBALANCE = TokenBalanceFrom.Etherscan
 const ETHERPRICE = 3500 // TODO get it from somewhere
+const DATE_TODAY = new Date()
 
 // MAIN FUNCTION
 
@@ -26,39 +21,57 @@ async function start() {
         .catch((error) => {
             console.log(error);
         });
-    var transactions = res.data.result
+    var resultEtherscan = res.data.result
+    console.log('all transactions=' + resultEtherscan.length);
+
     // Delete Duplicate "From Addresses"
-    await transactions.filter((v, i, a) => a.findIndex(t => (t.from === v.from)) === i)
+    var transactions = resultEtherscan.filter((v, i, a) => a.findIndex(t => (t.from === v.from)) === i)
+    console.log('delete duplicates=' + transactions.length);
 
-    for (const id in transactions as EtherscanTransaction) {
-        const transaction = transactions[id] as EtherscanTransaction
-        const today = new Date()
-        const timestamp = new Date(0)
-        timestamp.setUTCSeconds(parseInt(transaction.timeStamp));
-        var diffMs = (today.getTime() - timestamp.getTime()); // milliseconds between now & Christmas
-        var diffHrs = Math.round((diffMs % 86400000) / 3600000 * 100) / 100; // hours, round 2 decimals
-        if (transaction.input.substring(0, 10) === MINT_FUNCTION_HEX && diffHrs < 3 * 24) {
-            // console.log(transaction.input + "= MINT");
-            var balanceUSD = 0
-            if (GET_TOKENBALANCE == TokenBalanceFrom.Etherscan) {
-                balanceUSD = await getBalanceEtherscan(transaction.from)
-            } else {
+    // Filter for MINT Fuction and Within Timespan
+    transactions = transactions.filter((transaction) => {
+        var diffHrs = getDiffHours(transaction.timeStamp); // hours, round 2 decimals
+        return transaction.input.substring(0, 10) === MINT_FUNCTION_HEX && diffHrs < 3 * 24
+    })
+    console.log('MINT + Timespan=' + transactions.length);
 
-            }
-            if (balanceUSD > 10000) {
-                var balanceRounded = Math.round(balanceUSD) // round
-                console.log(`${diffHrs} Hours ago |  ${balanceRounded.toString()} USD (ETH ${balanceRounded / ETHERPRICE})|  MINT() ${transaction.from}`)
+    var transactionsArray = transactions.map(transaction => {
+        return transaction.from
+    });
+
+    console.log(transactionsArray);
+
+    var url = `https://api.zapper.fi/v1/protocols/tokens/balances`
+    var request = {
+        params: {
+            addresses: transactionsArray,
+            api_key: "96e0cc51-a62e-42ca-acee-910ea7d2a241",
+            network: "ethereum",
+            newBalances: true
+        }
+    }
+    await axios.get(url, request).then(res => {
+        var accountBalances = res.data
+        // for loop because of dynamic key in JSON
+        for (var ethAddress in accountBalances) {
+            var acc = accountBalances[ethAddress]
+            var totalUsdBalance = acc.meta[0].value;
+            if (totalUsdBalance > 10000) {
+                var balanceRounded = Math.round(totalUsdBalance) // round
+                // get diffHours of an Account
+                var diffHrsArray = resultEtherscan
+                    .filter(trans => trans.from == ethAddress)
+                    .map(trans => getDiffHours(trans.timeStamp))
+                console.log(`[${diffHrsArray}] Hours ago |  ${balanceRounded.toString()} USD (${Math.round(totalUsdBalance / ETHERPRICE)} ETH)|  MINT() ${ethAddress}`)
             }
         }
+    })
+}
 
-
-    }
-
-
-    async function getBalanceEtherscan(fromAddress: string): Promise<number> {
-        var balance = await web3.eth.getBalance(fromAddress)
-        var ether = web3.utils.fromWei(balance, "ether")
-        return ether * ETHERPRICE;
-    }
-
+function getDiffHours(timeStamp: string) {
+    const date = new Date(0);
+    date.setUTCSeconds(parseInt(timeStamp));
+    var diffMs = (DATE_TODAY.getTime() - date.getTime()); // milliseconds between now & Christmas
+    var diffHrs = Math.round((diffMs % 86400000) / 3600000 * 100) / 100; // hours, round 2 decimals
+    return diffHrs;
 }
